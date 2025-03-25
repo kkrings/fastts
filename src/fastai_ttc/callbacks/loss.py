5c1d5f84e29d5a76eda68f1d95bea87fcb0a64e2
Kai Krings <kai.krings@posteo.de>
2025-03-25 08:06:43 +0000

    Providing implementation for loss callback

--- src/fastai_ttc/callbacks/loss.py
+++ src/fastai_ttc/callbacks/loss.py
@@ -1,5 +1,17 @@
+import torch
 from fastai.text.all import Callback  # type: ignore
 
 
 class LossFromModel(Callback):  # type: ignore
-    ...
+    def __init__(self) -> None:
+        self._loss_from_model: torch.Tensor | None = None
+
+    def after_pred(self) -> None:
+        self._loss_from_model = self.pred.loss
+
+    def after_loss(self) -> None:
+        if self._loss_from_model is None:
+            return
+
+        self.learn.loss_grad = self._loss_from_model
+        self.learn.loss = self._loss_from_model.clone()
